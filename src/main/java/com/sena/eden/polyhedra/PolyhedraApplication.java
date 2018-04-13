5cd42d8171b51403d2da4c186e1e861d9234508a
Fabian Lopez <fabian.lopez@kinja.com>
2018-04-13 18:20:44 -0400

    incluye springboot en el proyecto

---
+++ src/main/java/com/sena/eden/polyhedra/PolyhedraApplication.java
@@ -0,0 +1,12 @@
+package com.sena.eden.polyhedra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyhedraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyhedraApplication.class, args);
+	}
+}
